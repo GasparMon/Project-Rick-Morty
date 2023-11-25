1810cf50a0ad7b65f7d08ffd9b673c7cc85ab525
GasparMon <imoncivaez@gmail.com>
2023-11-25 03:27:54 +0100

    update

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from "axios";
 import {useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "../Detail/Detail.css"
 
 export default function Detail(props) {
     
@@ -27,14 +28,34 @@ export default function Detail(props) {
     //console.log(character)
 
   return (
+    <div>
+       <h1>Informacion <span id="info" class="material-symbols-outlined">info</span>
+</h1>
+
     <div id="div-detail">
-        <h1>Detalle</h1>
-        <h2>Name: {character.name}</h2>
-        <h2>Status: {character.status}</h2>
-        <h2>Species: {character.species}</h2>
-        <h2>Gender: {character.gender}</h2>
-        <h2>Origin: {character.origin?.name}</h2>
-        <img src={character.image} alt={character.name} />
+   <Link id="link" to="/home"> <span class="material-symbols-outlined">arrow_back_ios_new</span></Link>
+        <div id="inner-detail">
+        <img id="detail-img" src={character.image} alt={character.name} />
+        <h2>{character.name}</h2>
+        </div>
+        <div id="inner-detail-text">
+        <h2>ID - {character.id}</h2>
+        <h2>Status - {character.status}
+        {character.status === 'Alive' &&  <span id="live"class="material-symbols-outlined">settings_accessibility</span>}
+        {character.status === 'Dead' &&  <span id="dead" class="material-symbols-outlined">skull</span>}
+        {character.status === 'unknown' &&  <span  id="unknown" class="material-symbols-outlined">help</span>}
+        </h2>
+        <h2>Gender - {character.gender}
+        {character.gender === 'Male' &&  <span id="Male" class="material-symbols-outlined">male</span>}
+        {character.gender === 'Female' &&  <span id="Female" class="material-symbols-outlined">female</span>}
+        {character.gender === 'unknown' &&  <span  id="unknown" class="material-symbols-outlined">help</span>}
+        </h2>
+        <h2>Species - {character.species}</h2>
+        <h2>Origin - {character.origin?.name}</h2>
+        <h2>Location - {character.location?.name}</h2>
+        </div>
+       
+    </div>
     </div>
   );
 }
--- src/components/Undefined/Undefined.jsx
+++ src/components/Undefined/Undefined.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "../Undefined/Undefined.css"
 
 export default function Undefined(props){
 
 
    return(
     <div id = "div-undefined">
-        <h1>ERROR 404</h1>
+        <h1 id="h1">Error 404 - ¡Wubba Lubba Dub Dub!</h1>
+    <p>¡Ups! Parece que esta dimensión no contiene la página que estás buscando. Tal vez esté en una realidad alternativa. Mientras tanto, disfruta de este momento de caos cósmico.</p>
+    <p>Recuerda, en el multiverso, los errores 404 son solo otra forma de entretenimiento para Rick.</p>
+    <img src="src/img/error404.png" alt="rick-404" id="rick-404"></img>
+    <img src="src/img/error404_nd.png" alt="nd-404" id="nd-404"></img>
     </div>
    ) 
 }
