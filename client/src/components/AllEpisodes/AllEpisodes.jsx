84b50474878978af43603f199e1eed6ad6c0777c
GasparMon <imoncivaez@gmail.com>
2024-01-25 19:09:45 +0100

    update

--- client/src/components/AllEpisodes/AllEpisodes.jsx
+++ client/src/components/AllEpisodes/AllEpisodes.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import "./Allepisodes.modules.css";
+import "./AllEpisodes.css";
 import { useDispatch, useSelector } from "react-redux";
 import { allEpisodes } from "../../redux/actions";
 import Episode from "../Episode/Episode";
